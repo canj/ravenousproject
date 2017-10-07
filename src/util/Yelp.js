5410799b159426f69ce8f593424296b1e6088a8e
Matt Cangialosi <mc88nj@gmail.com>
2017-10-07 13:16:23 -0400

    After project 4

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,33 +1,33 @@
 const clientId = 'I9AD2IXVaVlXJXQldM4gZA'
 const secret = 'XvmYLsnwp90M9MvGk0kbDbMLJChCJHjQTfybWYKBfcsGT5zTN1vL8DB8VW1vxB65'
-const accessToken = ''
+let accessToken;
 
 const Yelp = {
-  getAccessToken(
+  getAccessToken() {
     if (accessToken) {
       return new Promise(resolve => resolve(accessToken));
     }
-    return fetch(
-      'https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=`${clientId}``&client_secret=`${secret}`',
-      {method : 'POST'}).then(response => {
-        response.json();
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {
+      method : 'POST'
+    }).then(response => {
+        return response.json();
     }).then(jsonResponse => {
-      access_token = jsonResponse.access_token;
+      accessToken = jsonResponse.access_token;
     });
-  ),
+  },
 
   search(term, location, sortBy) {
     return Yelp.getAccessToken().then(() => {
-      return fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=`${this.term}`&location=`${this.location}`&sort_by=`${this.sortBy}`',
-        {
-          headers: {Authorization: `Bearer ${accessToken}`}
-        });
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${this.term}&location=${this.location}&sort_by=${this.sortBy}`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+      });
     }).then(response => {
-      response.json();
+      return response.json();
     }).then(jsonResponse => {
-      if (json.Response.businesses) {
-        return jsonResponse.businesses.map(business =>
-        {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
             id: business.id,
             imageSrc: business.image_url,
             name: business.name,
@@ -35,15 +35,13 @@ const Yelp = {
             city: business.location.city,
             state: business.location.state,
             zipCode: business.location.zip_code,
-            category: business.categories,
+            category: business.categories[0].title,
             rating: business.rating,
             reviewCount: business.review_count
-        }
-      );
+        }));
       }
     });
   }
-
 };
 
 export default Yelp;
